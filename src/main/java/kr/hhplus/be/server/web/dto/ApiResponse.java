9a07b46edc25f64b2bacc18b9ea814192f5e53a6
yoonsang Lee <ysle@pm.me>
2025-07-20 00:10:45 +0900

    refac: ApiResponse ctor -> lombok AllArgs-

--- src/main/java/kr/hhplus/be/server/web/dto/ApiResponse.java
+++ src/main/java/kr/hhplus/be/server/web/dto/ApiResponse.java
@@ -1,18 +1,14 @@
 package kr.hhplus.be.server.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ApiResponse<T> {
     private String message;
     private String code;
     private T data;
-
-    public ApiResponse(String message, String code, T data) {
-        this.message = message;
-        this.code = code;
-        this.data = data;
-    }
 }
