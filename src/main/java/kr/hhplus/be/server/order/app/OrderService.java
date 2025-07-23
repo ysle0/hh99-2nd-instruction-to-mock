4c74b89a32b20f81743f84b8046d00c13cbf4ad7
yoonsang Lee <ysle@pm.me>
2025-07-24 08:50:45 +0900

    chore: order 미사용 파일 정리 및 domain repository 로 변경

--- src/main/java/kr/hhplus/be/server/order/app/OrderService.java
+++ src/main/java/kr/hhplus/be/server/order/app/OrderService.java
@@ -1,17 +1,21 @@
 package kr.hhplus.be.server.order.app;
 
-import kr.hhplus.be.server.order.infra.OrderJpaRepository;
+import kr.hhplus.be.server.order.domain.OrderRepository;
 import kr.hhplus.be.server.order.presentation.dto.OrderProductResponse;
+import kr.hhplus.be.server.user.wallet.domain.WalletRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
-    private final OrderJpaRepository orderRepo;
+    private final OrderRepository orderRepo;
+    private final WalletRepository walletRepo;
 
     public OrderService(
-            OrderJpaRepository or
+            OrderRepository orderRepo,
+            WalletRepository walletRepo
     ) {
-        this.orderRepo = or;
+        this.orderRepo = orderRepo;
+        this.walletRepo = walletRepo;
     }
 
     public OrderProductResponse orderProduct(long userID, long productID, int quantity) {
